a01c1fe3731d0b655eb1ab16fb8c50d743153872
Petbys <92719116+Petbys@users.noreply.github.com>
2021-10-18 13:21:41 +0200

    edited

--- MA4_2.py
+++ MA4_2.py
@@ -1,15 +1,19 @@
 #!/usr/bin/env python3
 
 from integer import Integer
-
+import matplotlib
+from time import perf_counter as pc
 
 
 
 def main():
-	f = Integer(5)
-	print(f.get())
-	f.set(7)
-	print(f.get())
-
+	start = pc()
+	f = Integer(1)
+	for i in range(10):
+		print(f.fib(i))
+		f.set(i)
+		print(f.get())
+	end = pc()
+	print(f'tid = {end-start}')
 if __name__ == '__main__':
 	main()
