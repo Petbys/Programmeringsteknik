50fac40a553e076939d3e0041038850e5946208e
Petbys <92719116+Petbys@users.noreply.github.com>
2021-10-18 13:02:09 +0200

    changed

--- integer.cpp
+++ integer.cpp
@@ -5,15 +5,29 @@ class Integer{
 	public:
 		Integer(int);
 		int get();
+		int fib();
 		void set(int);
 	private:
 		int val;
+		int _fib(int);
 	};
  
 Integer::Integer(int n){
 	val = n;
 	}
- 
+
+int Integer::fib(){ 
+	return _fib(int val)
+	}
+
+int Integer::_fib(n)
+{
+    if (n <= 1)
+        return n;
+    return _fib(n-1) + _fib(n-2);
+}
+
+// This code is cont
 int Integer::get(){
 	return val;
 	}
@@ -25,6 +39,7 @@ void Integer::set(int n){
 
 extern "C"{
 	Integer* Integer_new(int n) {return new Integer(n);}
+	int Integer_fib(Integer* integer) {return integer->fib();}
 	int Integer_get(Integer* integer) {return integer->get();}
 	void Integer_set(Integer* integer, int n) {integer->set(n);}
 	void Integer_delete(Integer* integer){
