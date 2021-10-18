fca595e8dd920819967e4531b78175b34aa16bd7
Petbys <92719116+Petbys@users.noreply.github.com>
2021-10-18 14:48:50 +0200

    edit

--- MA4_2.py
+++ MA4_2.py
@@ -1,19 +1,37 @@
 #!/usr/bin/env python3
 
 from integer import Integer
-import matplotlib
+import matplotlib as plt
 from time import perf_counter as pc
+
+def fib_py(n):
+	if n <= 1:
+		return n
+	else:
+		return(fib_py(n-1) + fib_py(n-2))
 
 
 
 def main():
-	start = pc()
+	
+	tidpy = []
+	fibpy = []
+	tidc = []
+	fibc = []
+	startpy = pc()
 	f = Integer(1)
-	for i in range(10):
-		print(f.fib())
+	for i in range(30 ,45):
+		fibpy.append(fib_py(i))
+		tidpy.append(pc()-startpy)
+	startc = pc
+	for i in range(30,45):
 		f.set(i)
-		print(f.get())
+		fibc.append(f.fib())
+		tidc.append(pc()-startc)
+		
 	end = pc()
-	print(f'tid = {end-start}')
+	#print(f'tid = {end-start}')
+	plt.plot(tidpy,fibpy,tidc,fibc)
+	plt.show()
 if __name__ == '__main__':
 	main()
